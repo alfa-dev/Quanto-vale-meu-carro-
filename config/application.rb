e15163d75cf4c2383e86534f362c76e4280067a6
alfa-dev <rafael.mediario@gmail.com>
2019-07-27 12:27:16 -0300

    Config sinatra to show index page on app/views path

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'pathname'
+
+set :root do
+  Pathname.new File.realpath(File.join(File.dirname(__FILE__), '..')) << '/app'
+end
 
 get '/' do
-  'Hello world!'
+  erb :index
 end
