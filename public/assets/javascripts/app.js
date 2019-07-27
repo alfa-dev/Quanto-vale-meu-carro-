59a8be6e2b881b3e0e7a16c574a5bd2246c409a3
alfa-dev <rafael.mediario@gmail.com>
2019-07-27 15:50:03 -0300

    Store selected car

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -20,19 +20,29 @@
   };
 
   var render_brands_selector = function() {
-    var template       = document.querySelector('#car_brand_selector');
+    var template       = doc.querySelector('#car_brand_selector');
     var sectionToShow  = template.content.cloneNode(true);
 
-    var brand_selector = sectionToShow.querySelector('#brand_selector');
+    var brand_selector      = sectionToShow.querySelector('#brand_selector');
+    var brand_selector_form = sectionToShow.querySelector('#brand_selector_form');
 
     volanty.car_brands.map(function(brand){
       brand_selector.appendChild(create_option(brand));
     });
 
+    var brand_selector_form = sectionToShow.querySelector('#brand_selector_form');
+    brand_selector_form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      volanty.selected_car_brand = volanty.car_brands.filter(function(brand){
+        return (brand.id == brand_selector.value)
+      })[0];
+      return false;
+    });
+
     doc.body.appendChild(sectionToShow);
 
     function create_option(brand) {
-      var option = document.createElement('option');
+      var option = doc.createElement('option');
       option.value = brand.id;
       option.innerText = brand.name;
 
