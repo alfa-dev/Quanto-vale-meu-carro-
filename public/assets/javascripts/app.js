4ab7b8784790b11e8063466ca0e63457c9d2e99d
alfa-dev <rafael.mediario@gmail.com>
2019-07-27 15:35:31 -0300

    Render brands on selector

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -1,18 +1,44 @@
 (function(global, doc) {
 
   var volanty = global.volanty = global.volanty || {};
-  var app_start = function() {
-    load_car_brands();
-  }
 
-  var load_car_brands = function() {
+  var app_start = function() {
+    load_car_brands(render_brands_selector);
+  };
+
+  var load_car_brands = function(callback) {
     var api = new Api();
 
     api.fetch('/carro_marcas.json');
-    api.ok = function(data){
+
+    api.ok = function(data) {
       volanty.car_brands = data;
+
+      if(callback)
+        callback();
     }
-  }
+  };
+
+  var render_brands_selector = function() {
+    var template       = document.querySelector('#car_brand_selector');
+    var sectionToShow  = template.content.cloneNode(true);
+
+    var brand_selector = sectionToShow.querySelector('#brand_selector');
+
+    volanty.car_brands.map(function(brand){
+      brand_selector.appendChild(create_option(brand));
+    });
+
+    doc.body.appendChild(sectionToShow);
+
+    function create_option(brand) {
+      var option = document.createElement('option');
+      option.value = brand.id;
+      option.innerText = brand.name;
+
+      return option;
+    }
+  };
 
   doc.addEventListener("DOMContentLoaded", app_start);
 
