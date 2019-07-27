64a1790f00d8fbb1b417e7e61bd0de5afbed11f4
alfa-dev <rafael.mediario@gmail.com>
2019-07-27 16:50:10 -0300

    Remove the specification about 'car' in variable, tags and functions names. Because the project scope, will only support cars

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -3,16 +3,16 @@
   var volanty = global.volanty = global.volanty || {};
 
   var app_start = function() {
-    load_car_brands(render_brands_selector);
+    load_brands(render_brands_selector);
   };
 
-  var load_car_brands = function(callback) {
+  var load_brands = function(callback) {
     var api = new Api();
 
     api.fetch('/carro_marcas.json');
 
     api.ok = function(data) {
-      volanty.car_brands = data;
+      volanty.brands = data;
 
       if(callback)
         callback();
@@ -20,22 +20,23 @@
   };
 
   var render_brands_selector = function() {
-    var template       = doc.querySelector('#car_brand_selector');
+    var template       = doc.querySelector('#brand');
     var sectionToShow  = template.content.cloneNode(true);
 
-    var brand_selector      = sectionToShow.querySelector('#brand_selector');
-    var brand_selector_form = sectionToShow.querySelector('#brand_selector_form');
+    var brand_selector = sectionToShow.querySelector('#brand_selector');
+    var brand_form     = sectionToShow.querySelector('#brand_form');
 
-    volanty.car_brands.map(function(brand){
+    volanty.brands.map(function(brand){
       brand_selector.appendChild(create_option(brand));
     });
 
-    var brand_selector_form = sectionToShow.querySelector('#brand_selector_form');
-    brand_selector_form.addEventListener('submit', function(e) {
+    brand_form.addEventListener('submit', function(e) {
       e.preventDefault();
-      volanty.selected_car_brand = volanty.car_brands.filter(function(brand){
-        return (brand.id == brand_selector.value)
+
+      volanty.selected_brand = volanty.brands.filter( function(brand){
+        return (brand.id == brand_selector.value);
       })[0];
+
       return false;
     });
 
