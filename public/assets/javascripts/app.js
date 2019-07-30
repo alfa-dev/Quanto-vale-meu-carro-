467847920f6644da7306d5b435588433bb653f0e
alfa-dev <rafael.mediario@gmail.com>
2019-07-29 22:18:01 -0300

    Refactor way how to render the sections

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -1,6 +1,8 @@
 (function(global, doc) {
 
-  var volanty = global.volanty = global.volanty || {};
+  var volanty   = global.volanty = global.volanty || {};
+
+  volanty.container = document.querySelector("#container")
 
   var app_start = function() {
     load_data('/carro_marcas.json', function(data){
@@ -13,20 +15,20 @@
   var render_chosen_car = function() {
     var sectionToShow = get_template('chosen_car');
 
-    var name = sectionToShow.querySelector("#car_name");
-    var fuel = sectionToShow.querySelector("#car_fuel");
-    var year = sectionToShow.querySelector("#car_year");
-    var price = sectionToShow.querySelector("#car_price");
+    var name      = sectionToShow.querySelector("#car_name");
+    var fuel      = sectionToShow.querySelector("#car_fuel");
+    var year      = sectionToShow.querySelector("#car_year");
+    var price     = sectionToShow.querySelector("#car_price");
     var reference = sectionToShow.querySelector("#car_reference");
 
-    name.textContent = volanty.chosen_car.name;
-    fuel.textContent = volanty.chosen_car.combustivel;
-    year.textContent = volanty.chosen_car.ano_modelo;
-    price.textContent = volanty.chosen_car.preco;
+    name.textContent      = volanty.chosen_car.name;
+    fuel.textContent      = volanty.chosen_car.combustivel;
+    year.textContent      = volanty.chosen_car.ano_modelo;
+    price.textContent     = volanty.chosen_car.preco;
     reference.textContent = volanty.chosen_car.referencia;
 
-    doc.body.appendChild(sectionToShow);
-  }
+    render_section(sectionToShow);
+  };
 
   var render_years_selector = function() {
     var sectionToShow = get_template('year');
@@ -42,7 +44,7 @@
       e.preventDefault();
 
       volanty.selected_year = volanty.years.filter( function(year){
-        return (year.id == model_selector.value);
+        return (year.id == year_selector.value);
       })[0];
 
       load_data('/21_4828_2013-1.json', function(data) {
@@ -54,8 +56,8 @@
       return false;
     });
 
-    doc.body.appendChild(sectionToShow);
-  }
+    render_section(sectionToShow);
+  };
 
   var render_models_selector = function() {
     var sectionToShow = get_template('model');
@@ -83,8 +85,8 @@
       return false;
     });
 
-    doc.body.appendChild(sectionToShow);
-  }
+    render_section(sectionToShow);
+  };
 
   var render_brands_selector = function() {
     var sectionToShow = get_template('brand');
@@ -112,14 +114,34 @@
       return false;
     });
 
-    doc.body.appendChild(sectionToShow);
+    render_section(sectionToShow);
+  };
+
+  var render_section = function(section) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    container.appendChild(section);
+    afterRender();
+  };
+
+  function afterRender() {
+    var submits = document.querySelectorAll('form button[type="submit"]');
+
+    submits.forEach( function(submit) {
+      submit.form.addEventListener('submit', function(){
+        submit.textContent = 'Aguarde...';
+        submit.setAttribute('disabled', true);
+      });
+    });
   };
 
   var get_template = function(action) {
     var template = doc.querySelector('#' + action + '_template');
 
     return template.content.cloneNode(true);
-  }
+  };
 
   var load_data = function(url, callback) {
     var api = new Api();
@@ -136,7 +158,7 @@
     option.innerText = brand.name;
 
     return option;
-  }
+  };
 
   doc.addEventListener("DOMContentLoaded", app_start);
 
