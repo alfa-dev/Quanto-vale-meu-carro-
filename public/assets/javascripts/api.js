105ae60b0359517126cee5ef2f698e3c6a21bb23
alfa-dev <rafael.mediario@gmail.com>
2019-07-30 00:26:45 -0300

    Remove fetch function from Api

--- public/assets/javascripts/api.js
+++ public/assets/javascripts/api.js
@@ -1,14 +1,5 @@
-function Api(options) {
-
-  var options = options || {};
-  var init = {};
-
-  init['headers']     = options.headers     || { 'Accept': 'application/json', 'Content-Type': 'application/json' };
-  init['credentials'] = options.credentials || "same-origin";
-
-  this.init = init;
-
-  this.fields = options.fields || null;
+function Api(url) {
+  this.url = url || null;
 }
 
 Api.prototype.get = function(url) {
@@ -39,24 +30,4 @@ Api.prototype.XMLHttpRequest = function() {
     }
   }
   xhr.onerror = function(data) { self.error(data); }
-}
-
-Api.prototype.fetch = function() {
-  var self = this;
-
-  if( self.fields )
-    self.init.body = JSON.stringify( self.fields );
-
-  fetch(this.url, self.init)
-    .then(function(data){
-      if (data.statusText == "Unauthorized") self.unauthorized();
-      if (data.statusText == "Not Found")    self.notFound();
-      if (data.statusText == "OK") {
-        return data.json();
-      }
-    }).then(function(res){
-      self.ok(res);
-    }).catch(function(e){
-      throw e;
-    });
 }
